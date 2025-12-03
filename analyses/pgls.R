21649210a81d85348edbde91cf9d60fe025f3a2a
wangxm-forest <wangxiaomao0209@gmail.com>
2025-12-03 10:05:16 -0800

    code for doing pgls analysis

--- analyses/pgls.R
+++ analyses/pgls.R
@@ -1,40 +1,136 @@
-####PGLS####
-##started by Mao##
-##Nov-28-2025##
+#### Phylogeny logistic regression ####
+## Started by Mao ##
+## Nov-28-2025 ##
 
 library(caper)
 library(geiger)
+library(phylolm)
+library(brglm2)
+library(detectseparation)
 
-# housekeeping
-rm(list=ls()) 
+rm(list = ls())
 options(stringsAsFactors = FALSE)
 
-# working directory
 setwd("C:/PhD/Project/PhD_thesis/mast_trait")
 
-# read in the clean data and phylogeny tree
-d <- read.csv("data/cleanSilvics.csv", header = TRUE)
+
+# Load & prepare Data
+d <- read.csv("data/cleanSilvics.csv")
 phytree <- read.tree("output/silvicsPhylogenyFull.tre")
+
 d$latbi <- gsub(" ", "_", d$latbi)
-# remove rows with mastEvent data being NA
 d <- d[!is.na(d$mastEvent), ]
-# Make a subset of conifers only
-conifer <- subset(d, familyName %in% c("Pinaceae","Taxodiaceae"))
-# Make a subset of angiosperm only
-angio <- subset(d, !(familyName %in% c("Pinaceae","Taxodiaceae")))
+d$mastEvent <- ifelse(d$mastEvent == "Y", 1, 0)
 
-# Prune the tree for conifer species only
-phyconifer<-drop.tip(phytree, setdiff(phytree$tip.label, conifer$latbi))
-phyangio <- drop.tip(phytree, setdiff(phytree$tip.label, angio$latbi))
+conifer <- d[d$familyName %in% c("Pinaceae", "Taxodiaceae"), ]
+angio   <- d[!(d$familyName %in% c("Pinaceae", "Taxodiaceae")), ]
 
-# names match between the tree and the data frame
-name.check(conifer, phyconifer)
-name.check(angio, phyangio)
+phyconifer <- drop.tip(phytree, setdiff(phytree$tip.label, conifer$latbi))
+phyangio   <- drop.tip(phytree, setdiff(phytree$tip.label, angio$latbi))
 
-# Plot the tree
-plot(phyconifer,cex=0.4)
+rownames(conifer) <- conifer$latbi
+rownames(angio)   <- angio$latbi
 
-#
-compconifer <- comparative.data(phyconifer, conifer, names.col = "latbi", vcv = TRUE, na.omit = TRUE)
-########################Mast (Y/N)###########################################
-####
+# Make some functions
+
+# Extract key results from a phyloglm object
+tidy_phyloglm <- function(model) {
+  s <- summary(model)
+  coefs <- s$coefficients
+  out <- data.frame(
+    term = rownames(coefs),
+    estimate = coefs[, 1],
+    std_error = coefs[, 2],
+    z_value = coefs[, 3],
+    p_value = coefs[, 4],
+    alpha = model$alpha,
+    row.names = NULL
+  )
+  return(out)
+}
+
+# log10-transform + scale
+log_scale <- function(x) scale(log10(x))
+
+
+# Run model and attach metadata
+run_model <- function(formula, data, phy, method, trait_name) {
+  model <- phyloglm(formula, phy = phy, data = data, method = method)
+  tbl <- tidy_phyloglm(model)
+  tbl$trait <- trait_name
+  tbl$model_formula <- deparse(formula)
+  return(tbl)
+}
+
+
+# Prepare Trait Columns
+categorical_traits <- c("seedDispersal", "seedDormancy",
+                        "typeMonoOrDio", "droughtTolerance")
+
+for (t in categorical_traits) {
+  conifer[[t]] <- factor(conifer[[t]])
+  angio[[t]]   <- factor(angio[[t]])
+}
+
+# Continuous traits
+conifer$logSeedWeightStd <- log_scale(conifer$seedWeights)
+angio$logSeedWeightStd   <- log_scale(angio$seedWeights)
+
+conifer$logFruitStd <- log_scale(conifer$fruitSizeAve)
+angio$logFruitStd   <- log_scale(angio$fruitSizeAve)
+
+conifer$logSeedSizeStd <- log_scale(conifer$seedSizeAve)
+angio$logSeedSizeStd   <- log_scale(angio$seedSizeAve)
+
+
+# Model definitions
+model_list <- list(
+  list(name="Seed dispersal (conifer)", formula=mastEvent ~ seedDispersal, data=conifer, phy=phyconifer, method="logistic_MPLE"),
+  list(name="Seed dispersal (angio)",   formula=mastEvent ~ seedDispersal, data=angio,   phy=phyangio,   method="logistic_MPLE"),
+  
+  list(name="Seed dormancy (conifer)",  formula=mastEvent ~ seedDormancy, data=conifer, phy=phyconifer, method="logistic_MPLE"),
+  list(name="Seed dormancy (angio)",    formula=mastEvent ~ seedDormancy, data=angio,   phy=phyangio,   method="logistic_MPLE"),
+  
+  list(name="Mono/Dio (conifer)",       formula=mastEvent ~ typeMonoOrDio, data=conifer, phy=phyconifer, method="logistic_MPLE"),
+  list(name="Mono/Dio (angio)",         formula=mastEvent ~ typeMonoOrDio, data=angio,   phy=phyangio,   method="logistic_MPLE"),
+  
+  list(name="Seed weight (conifer)",    formula=mastEvent ~ logSeedWeightStd, data=conifer, phy=phyconifer, method="logistic_IG10"),
+  list(name="Seed weight (angio)",      formula=mastEvent ~ logSeedWeightStd, data=angio,   phy=phyangio,   method="logistic_IG10"),
+  
+  list(name="Fruit size (conifer)",     formula=mastEvent ~ logFruitStd, data=conifer, phy=phyconifer, method="logistic_IG10"),
+  list(name="Fruit size (angio)",       formula=mastEvent ~ logFruitStd, data=angio,   phy=phyangio,   method="logistic_IG10"),
+  
+  list(name="Seed size (conifer)",      formula=mastEvent ~ logSeedSizeStd, data=conifer, phy=phyconifer, method="logistic_IG10"),
+  list(name="Seed size (angio)",        formula=mastEvent ~ logSeedSizeStd, data=angio,   phy=phyangio,   method="logistic_IG10"),
+  
+  list(name="Oil content (angio)",      formula=mastEvent ~ oilContent, data=angio, phy=phyangio, method="logistic_IG10"),
+  
+  list(name="Leaf longevity (conifer)", formula=mastEvent ~ leafLongevity, data=conifer, phy=phyconifer, method="logistic_IG10"),
+  list(name="Leaf longevity (angio)",   formula=mastEvent ~ leafLongevity, data=angio,   phy=phyangio,   method="logistic_IG10"),
+  
+  list(name="Drought tol (conifer)",    formula=mastEvent ~ droughtTolerance, data=conifer, phy=phyconifer, method="logistic_IG10"),
+  list(name="Drought tol (angio)",      formula=mastEvent ~ droughtTolerance, data=angio,   phy=phyangio,   method="logistic_IG10")
+)
+
+
+# Run models
+
+results <- NULL
+
+for (m in model_list) {
+  cat("Running model:", m$name, "\n")
+  
+  tbl <- run_model(
+    formula = m$formula,
+    data    = m$data,
+    phy     = m$phy,
+    method  = m$method,
+    trait_name = m$name
+  )
+  
+  results <- rbind(results, tbl)
+}
+
+
+write.csv(results, "output/pglsResults.csv", row.names = FALSE)
+
